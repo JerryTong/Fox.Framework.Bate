94c9b2b325525c4e6b35e62c6321351a5e14cd1a
JerryTong <a866559@gmail.com>
2016-07-04 21:22:03 +0800

    修正xml檔案路徑問題。增加Extension。增加測試專案。

---
+++ Fox.Framework/Fox.Framework/Common/ObjectExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fox.Framework
+{
+    public static class ObjectExtension
+    {
+        public static int ToInt(this string obj, int defaultValue = 0)
+        {
+            if(obj == null)
+                return defaultValue;
+
+            int result = 0;
+            if (Int32.TryParse(obj.ToString(), out result))
+                return result;
+
+            return defaultValue;
+        }
+    }
+}
